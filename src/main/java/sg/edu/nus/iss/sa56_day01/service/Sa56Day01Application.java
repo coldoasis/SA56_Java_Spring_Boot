3ff18c9c4de70d36eb63afad70d93d9a4503b3a3
goodalways <johnyap9000@hotmail.com>
2023-06-08 14:59:12 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/sa56_day01/service/Sa56Day01Application.java
@@ -0,0 +1,12 @@
+package sg.edu.nus.iss.sa56_day01.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sa56Day01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sa56Day01Application.class, args);
+	}
+}
